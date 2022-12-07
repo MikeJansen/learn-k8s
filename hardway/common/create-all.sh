e5e39244ab39fd5e99c9b188f741164e75b9df73
Mike Jansen <mjans71@pm.me>
2022-12-06 23:29:50 -0500

    Fix bugs during create-all

--- hardway/common/create-all.sh
+++ hardway/common/create-all.sh
@@ -5,11 +5,14 @@ pushd ../step-03-provisioning
 terraform init
 terraform apply
 
+# let the compute settle to avoid possible SSH key race condition
+sleep 10
+
 cd ../step-04-certs
 ./gen-certs.sh
 
 cd ../step-05-config-files
-./gen-config-filess.sh
+./gen-config-files.sh
 
 cd ../step-06-encryption
 ./gen-ecnrypt.sh
